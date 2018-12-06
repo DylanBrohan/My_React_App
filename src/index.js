1337cafe2ebf069cf7ce470dde55f702de239d26
Dylan <dylan@Dylans-MacBook-Pro.local>
2018-12-06 10:20:19 +0000

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const users = [
+  {
+    name: "John",
+    age: 32,
+    image:
+      "https://www.atlassian.com/dam/jcr:13a574c1-390b-4bfb-956b-6b6d114bf98c/max-rehkopf.png"
+  },
+  {
+    name: "Test",
+    age: 32,
+    image:
+      "https://www.atlassian.com/dam/jcr:13a574c1-390b-4bfb-956b-6b6d114bf98c/max-rehkopf.png"
+  },
+  {
+    name: "Harry",
+    age: 54,
+    image:
+      "https://www.atlassian.com/dam/jcr:13a574c1-390b-4bfb-956b-6b6d114bf98c/max-rehkopf.png"
+  }
+];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// User Component
+function User(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p style={{ color: "green" }}>{props.age}</p>
+    </div>
+  );
+}
+
+class User2 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>{this.props.name}</h2>
+        <p style={{ color: "green" }}>{this.props.age}</p>
+        <img height="75" width="50" src={this.props.image} />
+      </div>
+    );
+  }
+}
+
+const userList = users.map(u => (
+  <User name={u.name} age={u.age} image={u.image} />
+));
+
+ReactDOM.render(<div>{userList}</div>, document.getElementById("root"));
